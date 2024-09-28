3e2b18d1ab16225d6e5091195855869ce4b384a4
stefantencate <stefan.ten.cate@me.com>
2024-09-28 10:57:00 +0200

    Internationalization added

--- home/models.py
+++ home/models.py
@@ -10,10 +10,12 @@ class HomePage(Page):
     hero_title = RichTextField(blank=True, verbose_name='Hero sectie titel')
     sub_title = RichTextField(blank=True, verbose_name='Sub titel')
 
+    hhw_titel = models.CharField(blank=True, max_length=255, verbose_name='HHW Paragraaf titel')
     hhw_paragraaf = RichTextField(blank=True, verbose_name="Hoe het werkt")
 
     content_panels = Page.content_panels + [
         FieldPanel('hero_title'),
         FieldPanel('sub_title'),
+        FieldPanel('hhw_titel'),
         FieldPanel('hhw_paragraaf'),
     ]
